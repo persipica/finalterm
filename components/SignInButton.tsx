8b5327ec5cef27401b2c0d99788a2c13f9938bd6
persipica <likekhs0107@naver.com>
2024-12-17 00:52:30 +0900

    1st

--- components/SignInButton.tsx
+++ components/SignInButton.tsx
@@ -27,8 +27,6 @@ export default function SignInButton() {
 
   return (
     <div className="container">
-      <h1 className="text-center text-4xl text-pink-500 mb-10">SIGN IN</h1>
-
       <div className="separator mb-8"></div>
 
       <button
@@ -38,18 +36,7 @@ export default function SignInButton() {
       >
         <Image src="/google-logo.png" height={30} width={30} alt="Google" />
         <span className="flex items-center justify-center relative -top-[38px]">
-          Sign in with Google
-        </span>
-      </button>
-
-      <button
-        className="github__btn flex items-center justify-center gap-4 w-full max-w-[680px] mx-auto mb-4 rounded-lg p-3"
-        onClick={() => handleSignIn('github')}
-        disabled={!csrfToken}
-      >
-        <Image src="/github-logo.png" height={30} width={30} alt="GitHub" />
-        <span className="flex items-center justify-center relative -top-[38px]">
-          Sign in with GitHub
+          Sign in
         </span>
       </button>
     </div>
